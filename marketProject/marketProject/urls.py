ef1800d1118bc3625f1e579d80d3c4acc7687899
jqk69 <achukannan174@gmail.com>
2025-04-19 16:39:34 +0530

    Merge branch 'main' of https://github.com/jqk69/djangoApp

--- marketProject/marketProject/urls.py
+++ marketProject/marketProject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myapp project.
+URL configuration for marketProject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from core.views import index,contact
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",index,name="index"),
+    path("contact/",contact,name="contact"),
+
 ]
